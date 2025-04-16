4e8f6a0b980a579cbe8c6dd16004ad4cc51bd5fb
Labiba M. Haque <127440017+LabibaHaque29@users.noreply.github.com>
2025-04-15 20:54:28 -0400

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,28 +1,26 @@
 export default function Layout(props) {
 
-    const {children} = props 
-    
+    const { children } = props
+
     const header = (
         <header>
-            <h1 className = "text-gradient">Stride Sync</h1>
-            <p><strong>The 30 Simple Training and Tracker</strong></p>
+            <h1 className="text-gradient">Stride Sync</h1>
+            <p><strong>The 30 Simple Workouts Program</strong></p>
         </header>
     )
 
     const footer = (
         <footer>
-            <p>Built By <a href = "https://www.labibahaque.netlify.app" target ="_blank">Labiba Haque</a> <br/> Styled with <a href ="https://www.fantacss.labibahaque.com" target ="_blank">FantaCSS</a> </p>
+            <p>Built by <a href="https://www.lhaque.com" target="_blank">Labiba Haque</a><br />Styled with <a href="https://www.fantacss.smoljames.com" target="_blank">FantaCSS</a></p>
         </footer>
     )
 
-    //https://www.LabibaHaque.netlify.app
 
     return (
         <>
             {header}
             {children}
             {footer}
-            
         </>
     )
 }
