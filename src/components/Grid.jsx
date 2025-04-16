23c0bab6c016bff9aee2ecfc8910257d967d2fbe
Labiba M. Haque <127440017+LabibaHaque29@users.noreply.github.com>
2025-04-15 20:40:06 -0400

    Components changes for Complete button bug fix

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,24 +1,22 @@
-import { useState, useEffect} from 'react'
-import {workoutProgram as training_plan} from '../utils/index.js'
+import { useState, useEffect } from 'react'
+import { workoutProgram as training_plan } from '../utils/index.js'
 import WorkoutCard from './WorkoutCard.jsx'
 
 export default function Grid() {
-
     const [savedWorkouts, setSavedWorkouts] = useState(null)
     const [selectedWorkout, setSelectedWorkout] = useState(null)
     const completedWorkouts = Object.keys(savedWorkouts || {}).filter((val) => {
         const entry = savedWorkouts[val]
         return entry.isComplete
-    } )
-    
+    })
 
     function handleSave(index, data) {
-        //save to local storage and modify the saved workouts  state
+        // save to local storage and modify the saved workouts state
         const newObj = {
-            ...savedWorkouts, 
+            ...savedWorkouts,
             [index]: {
                 ...data,
-                isComplete: !! data.isComplete || !!savedWorkouts?. [index]?. isComplete
+                isComplete: !!data.isComplete || !!savedWorkouts?.[index]?.isComplete
             }
         }
         setSavedWorkouts(newObj)
@@ -27,14 +25,14 @@ export default function Grid() {
     }
 
     function handleComplete(index, data) {
-        // complete a workout (we modify the completed status)
+        // complete a workout (so basically we modify the completed status)
         const newObj = { ...data }
         newObj.isComplete = true
         handleSave(index, newObj)
     }
 
     useEffect(() => {
-        if (!localStorage ) {return}
+        if (!localStorage) { return }
         let savedData = {}
         if (localStorage.getItem('brogram')) {
             savedData = JSON.parse(localStorage.getItem('brogram'))
@@ -43,20 +41,18 @@ export default function Grid() {
     }, [])
 
     return (
-
         <div className="training-plan-grid">
-            {Object.keys(training_plan).map((workout, workoutIndex) => {    
-                const isLocked = workoutIndex === 0 ? 
-                false : 
-                !completedWorkouts.includes(`${workoutIndex - 1}`)
+            {Object.keys(training_plan).map((workout, workoutIndex) => {
+                const isLocked = workoutIndex === 0 ?
+                    false :
+                    !completedWorkouts.includes(`${workoutIndex - 1}`)
                 console.log(workoutIndex, isLocked)
-        
-                const type = workoutIndex % 3 === 0 ? 
-                    'Push' : 
-                    workoutIndex % 3 === 1 ? 
-                    'Pull' : 
-                    'Legs'
 
+                const type = workoutIndex % 3 === 0 ?
+                    'Push' :
+                    workoutIndex % 3 === 1 ?
+                        'Pull' :
+                        'Legs'
 
                 const trainingPlan = training_plan[workoutIndex]
                 const dayNum = ((workoutIndex / 8) <= 1) ? '0' + (workoutIndex + 1) : workoutIndex + 1
@@ -69,7 +65,6 @@ export default function Grid() {
                         <i className='fa-solid fa-bolt'></i>
                     )
                 )
-                        
 
                 if (workoutIndex === selectedWorkout) {
                     return (
@@ -79,19 +74,17 @@ export default function Grid() {
 
                 return (
                     <button onClick={() => {
-                        if (isLocked) {return}
+                        if (isLocked) { return }
                         setSelectedWorkout(workoutIndex)
-                    }} className ={'card plan-card  ' + (isLocked ? 'inactive' : '')} key = {workoutIndex}>
-                        
-                        <div className ='plan-card-header'>
+                    }} className={'card plan-card  ' + (isLocked ? 'inactive' : '')} key={workoutIndex}>
+                        <div className='plan-card-header'>
                             <p>Day {dayNum}</p>
-
-                        {isLocked ? (
-                            <i className = 'fa-solid fa-lock'></i> 
-                        ) : (icon)}
+                            {isLocked ? (
+                                <i className='fa-solid fa-lock'></i>
+                            ) : (icon)}
                         </div>
 
-                        <div className ='plan-card-header'>
+                        <div className='plan-card-header'>
                             <h4><b>{type}</b></h4>
                         </div>
                     </button>
--- src/components/WorkoutCard.jsx
+++ src/components/WorkoutCard.jsx
@@ -2,45 +2,41 @@ import React, { useState } from "react"
 import Modal from "./Modal"
 import { exerciseDescriptions } from "../utils"
 
-
 export default function WorkoutCard(props) {
+    const { trainingPlan, workoutIndex, type, dayNum, icon, savedWeights, handleSave, handleComplete } = props
 
-    const {trainingPlan, workoutIndex, type, dayNum, icon, savedWeights, handleSave, handleComplete} = props 
-    const { warmup, workout } = trainingPlan || {}  
+    const { warmup, workout } = trainingPlan || {}
     const [showExerciseDescription, setShowExerciseDescription] = useState(null)
-    const[weights, setWeights] = useState(savedWeights || {})
+    const [weights, setWeights] = useState(savedWeights || {})
 
     function handleAddWeight(title, weight) {
         const newObj = {
-            ...weights, 
+            ...weights,
             [title]: weight
         }
         setWeights(newObj)
     }
 
-    return(
-        <div className ='workout-container'>
-        {showExerciseDescription && (
-            <Modal showExerciseDescription={showExerciseDescription} 
-            handleCloseModal={() => { 
-                setShowExerciseDescription(null)
-             }} />
-        
-        )}
-
-             <div className = 'workout-card card'>
-                <div className ='plan-card-header'>
+    return (
+        <div className="workout-container">
+            {showExerciseDescription && (
+                <Modal showExerciseDescription={showExerciseDescription} handleCloseModal={() => {
+                    setShowExerciseDescription(null)
+                }} />
+            )}
+            <div className="workout-card card">
+                <div className="plan-card-header">
                     <p>Day {dayNum}</p>
                     {icon}
                 </div>
-
-                 <div className ='plan-card-header'>
+                <div className="plan-card-header">
                     <h2><b>{type} Workout</b></h2>
-                 </div>
+                </div>
             </div>
 
-            <div className ='workout-grid'>
-                <div className='exercise-name'>
+
+            <div className="workout-grid">
+                <div className="exercise-name">
                     <h4>Warmup</h4>
                 </div>
                 <h6>Sets</h6>
@@ -49,32 +45,27 @@ export default function WorkoutCard(props) {
                 {warmup.map((warmupExercise, warmupIndex) => {
                     return (
                         <React.Fragment key={warmupIndex}>
-
                             <div className="exercise-name">
-                                 <p>{warmupIndex + 1}. {warmupExercise.name}</p>
-                                 <button onClick={() => {
+                                <p>{warmupIndex + 1}. {warmupExercise.name}</p>
+                                <button onClick={() => {
                                     setShowExerciseDescription({
                                         name: warmupExercise.name,
                                         description: exerciseDescriptions[warmupExercise.name]
                                     })
-                                 }} className="help-icon">
-                                    <i className="fa-regular fa-circle-question"/>
-                                 </button>
+                                }} className="help-icon">
+                                    <i className="fa-regular fa-circle-question" />
+                                </button>
                             </div>
                             <p className="exercise-info">{warmupExercise.sets}</p>
                             <p className="exercise-info">{warmupExercise.reps}</p>
-                            <input value = {weights[warmupExercise.name] || ' '} 
-                                    onChange={(e) => {
-                                        handleAddWeight(warmupExercise.name, e.target.value)
-                            }} className="weight-input" placeholder="14" />
-
+                            <input className="weight-input" placeholder="N/A" disabled />
                         </React.Fragment>
                     )
                 })}
             </div>
 
-            <div className ='workout-grid'>
-                <div className='exercise-name'>
+            <div className="workout-grid">
+                <div className="exercise-name">
                     <h4>Workout</h4>
                 </div>
                 <h6>Sets</h6>
@@ -83,40 +74,36 @@ export default function WorkoutCard(props) {
                 {workout.map((workoutExercise, wIndex) => {
                     return (
                         <React.Fragment key={wIndex}>
-
                             <div className="exercise-name">
-                                 <p>{wIndex + 1}. {workoutExercise.name}</p>
-                                 <button onClick={() => {
+                                <p>{wIndex + 1}. {workoutExercise.name}</p>
+                                <button onClick={() => {
                                     setShowExerciseDescription({
                                         name: workoutExercise.name,
                                         description: exerciseDescriptions[workoutExercise.name]
                                     })
-                                 }} className="help-icon">
-                                    <i className="fa-regular fa-circle-question"/>
-                                 </button>
+                                }} className="help-icon">
+                                    <i className="fa-regular fa-circle-question" />
+                                </button>
                             </div>
                             <p className="exercise-info">{workoutExercise.sets}</p>
                             <p className="exercise-info">{workoutExercise.reps}</p>
-                            <input value = {weights[workoutExercise.name]} onChange={(e) => {
-                                        handleAddWeight(workoutExercise.name, e.target.value)
+                            <input value={weights[workoutExercise.name] || ''} onChange={(e) => {
+                                handleAddWeight(workoutExercise.name, e.target.value)
                             }} className="weight-input" placeholder="14" />
-
                         </React.Fragment>
                     )
                 })}
             </div>
 
-            <div className="workout-buttons">
 
+            <div className="workout-buttons">
                 <button onClick={() => {
                     handleSave(workoutIndex, { weights })
-                }} >Save and Exit</button>
-
+                }}>Save & Exit</button>
                 <button onClick={() => {
                     handleComplete(workoutIndex, { weights })
                 }} disabled={Object.keys(weights).length !== workout.length}>Complete</button>
             </div>
-            
         </div>
     )
 }
